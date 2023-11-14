6f959876c0aa5f98799d128dfe42b6ad37bad6f6
Shafiqul Islam <www.shafiquebd@gmail.com>
2023-11-14 09:27:03 +0600

    added if else problem

--- lib/dart_new_feature/dart_final_class_explaination/final_class_example.dart
+++ lib/dart_new_feature/dart_final_class_explaination/final_class_example.dart
@@ -1,17 +1,21 @@
 void main() {}
 
-//The type 'B' must be 'base',
-// 'final' or 'sealed' because the supertype 'A' is 'final'.
+///The type 'B' must be 'base',
+/// 'final' or 'sealed' because the supertype 'A' is 'final'.
 final class A {}
 
-class B extends A {}
+base class B extends A {}
 
-class C implements A {}
-
+base class C implements A {}
+/// The class 'Vehicl' can't be extended outside 
+///of its library because it's a final 
+/// 1.[final] class can be extends from its own file
+/// 2.final class can be implement from its own file or library
+/// 3.final class can not be extends or implement from outside of it own file or library
+/// 4. a final,base,or sealed class can be extends or implement a final class 
 final class Vehicl {
   carMoveMent(int metre) {}
 }
-
  interface class M {
   
 }
--- lib/dart_new_feature/dart_interface/dart_interface_example.dart
+++ lib/dart_new_feature/dart_interface/dart_interface_example.dart
@@ -1,12 +1,17 @@
 void main(){
 
 }
-
+/// An [Interface] can not be inherit or
+///  extends from out side of its own library or file 
+/// an [Interface] can only implement out side of it own file or library
+/// an [interface] can extends and implements from its own file or library
+/// an [interface] can not contain [abstract] method because 
+/// it is not pure [interface]
+/// an [abstract interface class] is pur interface 
 interface class Vehicl{
   getSum(){
     
   }
-
 }
 
 class Motor extends Vehicl{
